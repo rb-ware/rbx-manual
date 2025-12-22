ae10ce3b2a15f57bcece6d836f9b303f8e05c66b
yeseulsim <rbware.yeseul@gmail.com>
2025-12-22 23:24:51 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print('test');
     return MaterialApp(
       title: 'RB X Manual',
       theme: AppTheme.light(),
